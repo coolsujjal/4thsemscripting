fced20881c3418a99c23342ee71c2e00cb58ca7a
coolsujjal <coolsujjal@gmail.com>
2024-08-11 16:08:40 +0545

    Labwork I done

---
+++ Labwork I/script.js
@@ -0,0 +1,7 @@
+// This is a single-line comment in JavaScript
+
+// Declaring a variable to store the message
+const message = "Hello, World!";
+
+// Outputting the message to the console
+console.log(message);
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 4thsemscripting
 Assignment's of 4th semester of scripting language.
+
+
+on sunday, shrawan 27 2081 Labwork I to submit
+
+
+
+
+
+
+
+
+
+
