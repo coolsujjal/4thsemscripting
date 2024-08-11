f71bbf85737486d5746111c5058029132e9e3075
coolsujjal <coolsujjal@gmail.com>
2024-08-11 20:33:19 +0545

    assignment completed

--- Labwork I/script.js
+++ Labwork I/script.js
@@ -2,3 +2,60 @@
 var helloW = "Hello, World!";
 
 console.log(helloW);
+
+/*
+Types of Operators
+
+1. Arithmetic Operator
+i.Addition + 
+ii. Subtraction -
+iii. Multiplication *
+iv. Divison /
+v. Modulous %
+vi. Exponentation **
+vii. Increment ++
+viii. Decrement --
+
+2. Assignment Operator
+i. Assignment =
+ii. Addition Assignment += etc..
+
+3. Comparison Operator
+i. Equal ==
+ii. Strict Equal ===
+iii. Not Equal !=
+iv. Greater than >
+v. Greater than or Equal to >=
+
+4. Logical Operator
+i. Logical AND &&
+ii. Logical OR ||
+iii. Logical NOT !
+
+5. Bitwise Operator
+i. AND &
+ii. OR |
+iii XOR ^
+iv. NOT ~
+v. Left Shift <<
+vi. Right Shift >>
+
+6. Ternary Operator
+? : 
+example : let result = (5>3) ? "Yes" : "No"
+
+7. Type Operator
+typeof 41; //"number"
+let arr = [];
+arr instance of Array; //true 
+
+8. String Operators
+i. Concatenation('+') :- joins two or more strings together
+
+9. Comma Operator
+let x = (1+2, 3+4); //x is 7
+
+10. Nullish Coalescing Operator ('??')
+let name = null;
+let defaultName = name ?? "Guest"; // Guest
+*/
